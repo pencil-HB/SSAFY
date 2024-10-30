846e7799f15cc60c04a8615dd6418787eea14df0
김혜빈 <bin5459@naver.com>
2024-10-30 17:54:34 +0900

    backup 1030

--- SpringMVCSsafyTest/src/main/java/com/mvc/service/MemberService.java
+++
@@ -1,8 +0,0 @@
-package com.mvc.service;
-
-import com.mvc.vo.Member;
-
-public interface MemberService {
-	Member check(Member u);
-	boolean registerMember(Member member);
-}
---
+++ board6-spring/src/main/java/com/ssafy/board/Board6SpringApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Board6SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Board6SpringApplication.class, args);
+	}
+
+}
